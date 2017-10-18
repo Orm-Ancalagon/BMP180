54c6a21aa33cabc8ed6b075bea85ccffd514f0d8
sam.attwell <sam.attwell@gmail.com>
2017-10-18 11:36:36 +0100

    updated with dtypes and reshaping of array

--- temppressurelogger.py
+++ temppressurelogger.py
@@ -1,18 +1,16 @@
 import Adafruit_BMP.BMP085 as BMP085 # Using the Adafruit BMP sensor library
 import time	
-import numpy 
+import numpy as np
 
 sensor = BMP085.BMP085() # Defines sensor from which to read
+
 t = time.asctime(time.localtime(time.time())) # Stores local time in t
-
 temperature = sensor.read_temperature()
-float(temperature)
-
 pressure = sensor.read_pressure()
-float(pressure)
 
-array = numpy.array(zip(temperature, pressure, t), 
-		dtype = [("temperature", float), ("pressure", float), ("t", "S16")]) 
+data = np.array([[temperature], [pressure], [t]])  
+np.dtype([("temperature", float), ("pressure", float), ("t", "S20")])
+#print(array.shape)
 
-numpy.savetxt("output.csv", array, fmt = ["%.2f",]*2 + ["%s"],  delimiter=",")
+np.savetxt("output.csv", data.reshape((1,3)), fmt = "%.2f %.2f %s", delimiter=",")
 
